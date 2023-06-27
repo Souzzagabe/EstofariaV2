e5039f6d8b4e4fb2395dd469e0e91aa56f2a52e2
Gabriel <souzzagabe@hotmail.com>
2023-06-27 19:32:38 -0300

    responsividade e navbar

---
+++ index.js
@@ -0,0 +1,27 @@
+const menuLinks = document.querySelectorAll('.navigation a[href^="#"]')
+
+
+function getDistanceFromThetop(element) {
+    const id = element.getAttribute("href")
+    return document.querySelector(id).offsetTop
+
+}
+
+function nativeScroll(distanceFromTheTop) {
+    window.scroll ({
+        top: distanceFromTheTop, 
+        behavior: "smooth",
+
+    })
+}
+
+function scrollToSection(event) {
+    event.preventDefault()
+    const distanceFromTheTop = getDistanceFromThetop(event.target) - 90
+    nativeScroll(distanceFromTheTop)
+
+}
+
+menuLinks.forEach((link) => {
+    link.addEventListener("click", scrollToSection)
+})
